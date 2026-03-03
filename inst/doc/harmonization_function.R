8931387cb2a5246e0c977e392e2d65c87839360d
Catalin Covaci <catalin.covaci@csic.es>
2026-03-03 21:20:03 +0000

    version 0.3.0

---
+++ inst/doc/harmonization_function.R
@@ -0,0 +1,11 @@
+## ----include = FALSE----------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ----setup--------------------------------------------------------------------
+library(whep)
+# Don't ask for credentials (read public files)
+googlesheets4::gs4_deauth()
+
